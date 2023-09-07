f827d9294da7a4f9fcc5aa458a5ea44bd1afb1d8
kate <khan02@qub.ac.uk>
2023-09-07 18:09:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("H?E?LL?o")
+calories = []
+
+with open("adventofcode.com_2022_day_1_input.txt") as file:
+    elves = []
+    elf = []
+    calories=[]
+    for line in file:
+        line = line.replace('\n', '')
+        if line:
+            line=int(line)
+            elf.append(line)
+        else:
+            calories.append(sum(elf))
+            elves.append(elf)
+            elf = []
+
+    print(max(calories))
