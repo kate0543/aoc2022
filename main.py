57e018abeb04dc14d3dfa08329a536e7881e80f3
kate <khan02@qub.ac.uk>
2023-09-07 18:17:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ with open("adventofcode.com_2022_day_1_input.txt") as file:
     for line in file:
         line = line.replace('\n', '')
         if line:
-            line=int(line)
+            line = int(line)
             elf.append(line)
         else:
             calories.append(sum(elf))
             elves.append(elf)
             elf = []
-
-    print(max(calories))
+    calories.sort()
+    print(max(calories), sum(calories[-3:]))
